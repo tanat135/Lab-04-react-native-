974d51d323a71914072165af309594f32e77f8ea
tanat165 <6210110135@psu.ac.th>
2021-07-12 09:59:06 +0700

    part 1-2

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+export default function App() {
+ const doIt = () => {
+ console.log("Hello from console")
+ }
+ return (
+ <View style={styles.container}>
+ <Text onPress={doIt}>Hello world</Text>
+ <StatusBar style="auto" />
+ </View>
+ );
+}
+const styles = StyleSheet.create({
+ container: {
+ flex: 1,
+ backgroundColor: '#fff',
+ alignItems: 'center',
+ justifyContent: 'center',
+ },
+});
