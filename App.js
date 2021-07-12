a83b8842be4d39f007be9d2e0cad51fb017c9182
tanat165 <6210110135@psu.ac.th>
2021-07-12 10:48:19 +0700

    part2

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
+
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
  return (
- <View style={styles.container}>
- <Text onPress={doIt}>Hello world</Text>
- <StatusBar style="auto" />
- </View>
+    <View style={styles.container}>
+    <Weather zipCode="90110"/>
+    <StatusBar style="auto" />
+    </View>
+   
  );
 }
+
 const styles = StyleSheet.create({
  container: {
  flex: 1,
@@ -20,3 +21,5 @@ const styles = StyleSheet.create({
  justifyContent: 'center',
  },
 });
+
+
