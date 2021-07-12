3c5696e0465bfd96fa3e72517286db06fa57a128
tanat165 <6210110135@psu.ac.th>
2021-07-12 11:26:01 +0700

    3

---
+++ components/Forecast.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { View, Text } from "react-native"
+
+export default function Forecast(props) {
+    return (
+    <View >
+    <Text>{props.main}</Text>
+    <Text>{props.description}</Text>
+    <View>
+    <Text>{props.temp}</Text>
+    <Text>°C</Text>
+    </View>
+    </View>
+    );
+   }
+   
--- components/Weather.js
+++ components/Weather.js
@@ -1,9 +1,21 @@
-import React from "react"
-import { Text } from "react-native"
+import React, { useState } from "react"
+import { View, Text, ImageBackground } from "react-native"
+import Forecast from "./Forecast";
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+    })
     return (
-    <Text>{props.zipCode}</Text>
+        <View>
+            <ImageBackground source={require('../bg.jpg')} style={StyleSheet.backdrop}>
+                <Text>Zip Code</Text>
+                <Text>{props.zipCode}</Text>
+                <Forecast {...forecastInfo} />
+            </ImageBackground>
+        </View>
     );
    }
    
